2e1d29de6427c0545b1b4b1a6a9cbe64df1e4a04
Alexey <33929372+ma9gyst@users.noreply.github.com>
2018-11-28 14:13:34 +0200

    add file

---
+++ Accounts.cs
@@ -0,0 +1,12 @@
+﻿namespace mygame
+{
+    public class Accounts
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Image { get; set; }
+
+        public Accounts AccountsId { get; set; }
+    }
+}
