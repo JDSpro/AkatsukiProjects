353eff5b8845b137830f7c3a6e6a0d76097ea400
JDSpro <kirilo98@gmail.com>
2018-11-30 15:00:54 +0200

    NewDesign

--- My Game/obj/Debug/My Game_Content.g.i.cs
+++ My Game/obj/Debug/My Game_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("milli.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("loginpictures.png")]
 
 
