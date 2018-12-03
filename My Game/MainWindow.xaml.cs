4afa12644cfb453e08c7fd405f4f46ec0c3e006e
Кирилл Гойда <kirilo98@gmail.com>
2018-12-03 22:01:58 +0200

    03.12.2018

--- My Game/MainWindow.xaml.cs
+++ My Game/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using MahApps.Metro.Controls;
 using MahApps.Metro.Controls.Dialogs;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -27,6 +28,61 @@ namespace My_Game
         public MainWindow()
         {
             InitializeComponent();
+
+            //SignInTabItemHeaderButton.MouseDown += SignInTabItemHeaderButton_MouseDown;
+            //SignUpTabItemHeaderButton.MouseDown += SignUpTabItemHeaderButton_MouseDown;
+
+            //SignInLabel.MouseEnter += SignInLabel_MouseEnter;
+            //SignUpLabel.MouseEnter += SignUpLabel_MouseEnter;
+            //SignInLabel.MouseLeave += SignInLabel_MouseLeave;
+            //SignUpLabel.MouseLeave += SignUpLabel_MouseLeave;
+
+            //EnterButton.MouseEnter += EnterButton_MouseEnter;
+
+            SignUpPicture.MouseDown += SignUpPicture_MouseDown;
+
+            FlayoutTabCotrol.SelectionChanged += FlayoutTabCotrol_SelectionChanged;
+        }
+
+        private void SignUpPicture_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    SignUpPicture.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+                }
+                catch (System.NotSupportedException)
+                {
+                    MessageBox.Show("Невернный формат изображения." , "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+        }
+
+        private void EnterButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            //EnterButton.Background = new SolidColorBrush(Colors.Black);
+            EnterButton.Foreground = new SolidColorBrush(Colors.White);
+        }
+
+        private void FlayoutTabCotrol_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (FlayoutTabCotrol.SelectedIndex == 0)
+            {
+                SignInTabItem.Background = new SolidColorBrush(Colors.White);
+                SignUpTabItem.Background = (System.Windows.Media.Brush)new BrushConverter().ConvertFrom("#41B1E1");
+                SignInUpFlayout.Header = "Вход";
+
+            }
+            else if (FlayoutTabCotrol.SelectedIndex == 1)
+            {
+                SignUpTabItem.Background = new SolidColorBrush(Colors.White);
+                SignInTabItem.Background = (System.Windows.Media.Brush)new BrushConverter().ConvertFrom("#41B1E1");
+                SignInUpFlayout.Header = "Регистрация";
+            }
         }
 
         private void LoginButton_Click(object sender, RoutedEventArgs e)
@@ -37,8 +93,45 @@ namespace My_Game
             flyout.CloseCommand = comm;
 
             LoginButton.Visibility = System.Windows.Visibility.Hidden;
+
             //DialogManager.ShowLoginAsync(this, "", "");
         }
+
+        //private void SignUpTabItemHeaderButton_MouseDown(object sender, MouseButtonEventArgs e)
+        //{
+        //    SignUpTabItemHeaderButton.Background = new SolidColorBrush(Colors.White);
+        //    SignInTabItemHeaderButton.Background = (System.Windows.Media.Brush)new BrushConverter().ConvertFrom("#41B1E1");
+        //}
+
+        //private void SignInTabItemHeaderButton_MouseDown(object sender, MouseButtonEventArgs e)
+        //{
+        //    Button btn = sender as Button;
+
+        //    SignInTabItemHeaderButton.Background = new SolidColorBrush(Colors.White);
+        //    btn.Background = (System.Windows.Media.Brush)new BrushConverter().ConvertFrom("#41B1E1");
+        //}
+
+        #region TabLabelMouseActions
+        //private void SignInLabel_MouseEnter(object sender, MouseEventArgs e)
+        //{
+        //    SignInTabItem.Background = new SolidColorBrush(Colors.White);
+        //}
+
+        //private void SignInLabel_MouseLeave(object sender, MouseEventArgs e)
+        //{
+        //    SignInTabItem.Background = (System.Windows.Media.Brush)new BrushConverter().ConvertFrom("#41B1E1");
+        //}
+
+        //private void SignUpLabel_MouseEnter(object sender, MouseEventArgs e)
+        //{
+        //    SignUpTabItem.Background = new SolidColorBrush(Colors.White);
+        //}
+
+        //private void SignUpLabel_MouseLeave(object sender, MouseEventArgs e)
+        //{
+        //    SignUpTabItem.Background = (System.Windows.Media.Brush)new BrushConverter().ConvertFrom("#41B1E1");
+        //}
+        #endregion
 
         private void Flyout_ContextMenuClosing(object sender, RoutedEventArgs e)
         {
