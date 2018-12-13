50b20a7475a9828c2839021ea372c311e30c193a
JDSpro <kirilo98@gmail.com>
2018-12-13 14:18:13 +0200

    1111

---
+++ My Game/Classes/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_Game.Classes
+{
+    class Game
+    {
+        Question question;
+
+        void Question()
+        {
+           // question
+        }
+    }
+}
