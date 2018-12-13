b676a8d3ad280f1d71ac2cec7851bd99450b5d9b
al00xey@gmail.com <al00xey@gmail.com>
2018-12-13 14:17:21 +0200

    question

--- My Game/Classes/Personal_Data_Acc.cs
+++ My Game/Classes/Personal_Data_Acc.cs
@@ -11,15 +11,10 @@ namespace My_Game
    public class Personal_Data_Acc
     {
         public int Id { get; set; }
-        //public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Patronymic { get; set; }
-        //[Index(IsUnique = true)]
-        //[StringLength(200)]
         public string Email{ get; set; }
         public byte[] Photo { get; set; }
-        public DateTime DateOfBirth { get; set; } = DateTime.Now;
-        
     }
 }
--- My Game/Classes/Utilities.cs
+++ My Game/Classes/Utilities.cs
@@ -16,27 +16,195 @@ namespace My_Game
 {
     public static class Utilities
     {
+        //Загрузка данных, для разработки(до релиза)
         public static void Seed()
         {
-            using (MyContext db = new MyContext())
+            using (MyContext context = new MyContext())
             {
                 try
                 {
-                    // создаю аккаунт
-                    Account JmixAcc = new Account
+
+                    #region 100 (6 вопросов)
+                    Question quest1 = new Question
                     {
-                        Login = "Игрок",
-                        //128-битный алгоритм хеширования md5
-                        Password = MD5Hash.GetMd5Hash("qwerty"),
-                        Personal = new Personal_Data_Acc
-                        {
-                            Photo = ImageToByte(@"Images\NoImage.png"),
-                        }
+                        Text = "Сколько букв в русском алфавите?",
+                        IssuePrice = 100,
+                        Answers = new List<Answer>
+                     {
+                         new Answer{ Text = "32", IsCorrect = false},
+                         new Answer{ Text = "33", IsCorrect = true},
+                         new Answer{ Text = "34", IsCorrect = false},
+                         new Answer{ Text = "35", IsCorrect = false}
+                     }
                     };
-                    // добавляю в бд
-                    db.Accounts.Add(JmixAcc);
-                    // db.Entry(JmixAcc).State = EntityState.Added;
-                    db.SaveChanges();
+                    Question quest2 = new Question
+                    {
+                        Text = "Операционная система от Microsoft называется...",
+                        IssuePrice = 100,
+                        Answers = new List<Answer>
+                     {
+                         new Answer{ Text = "Двери", IsCorrect = false},
+                         new Answer{ Text = "Форточки", IsCorrect = false},
+                         new Answer{ Text = "Плитки", IsCorrect = false},
+                         new Answer{ Text = "Окна", IsCorrect = true}
+                     }
+                    };
+                    Question quest3 = new Question
+                    {
+                        Text = "Эмблему какого учреждения украшает змея обвивающая чашу?",
+                        IssuePrice = 100,
+                        Answers = new List<Answer>
+                     {
+                         new Answer{ Text = "Зоопарк", IsCorrect = false},
+                         new Answer{ Text = "Зоомагазин", IsCorrect = false},
+                         new Answer{ Text = "Аптека", IsCorrect = true},
+                         new Answer{ Text = "Дворец бракосочетаний", IsCorrect = false}
+                     }
+                    };
+                    Question quest4 = new Question
+                    {
+                        Text = "Какого моря не существует?",
+                        IssuePrice = 100,
+                        Answers = new List<Answer>
+                     {
+                         new Answer{ Text = "Красного моря", IsCorrect = false},
+                         new Answer{ Text = "Белого моря", IsCorrect = false},
+                         new Answer{ Text = "Черного моря", IsCorrect = false},
+                         new Answer{ Text = "Сиреневого моря", IsCorrect = true}
+                     }
+                    };
+                    Question quest5 = new Question
+                    {
+                        Text = "С какого расстояния назначают пенальти в футболе?",
+                        IssuePrice = 100,
+                        Answers = new List<Answer>
+                     {
+                         new Answer{ Text = "12 метров", IsCorrect = false},
+                         new Answer{ Text = "11 метров", IsCorrect = true},
+                         new Answer{ Text = "10 метров", IsCorrect = false},
+                         new Answer{ Text = "9 метров", IsCorrect = false}
+                     }
+                    };
+                    Question quest6 = new Question
+                    {
+                        Text = "Как в математике называется верное равенство двух отношений?",
+                        IssuePrice = 100,
+                        Answers = new List<Answer>
+                     {
+                         new Answer{ Text = "Уравнение", IsCorrect = false},
+                         new Answer{ Text = "Выражение", IsCorrect = false},
+                         new Answer{ Text = "Пропорция", IsCorrect = true},
+                         new Answer{ Text = "Тождество", IsCorrect = false}
+                     }
+                    };
+                    #endregion
+                    #region 200 (6 вопросов)
+                    Question quest7 = new Question
+                    {
+                        Text = "В каком году был выпущен первый прототип портативного сотового телефона(Motorola DynaTAC)?",
+                        IssuePrice = 200,
+                        Answers = new List<Answer>
+                     {
+                         new Answer{ Text = "1964", IsCorrect = false},
+                         new Answer{ Text = "1973", IsCorrect = true},
+                         new Answer{ Text = "1982", IsCorrect = false},
+                         new Answer{ Text = "1987", IsCorrect = false},
+                     }
+                    };
+                    Question quest8 = new Question
+                    {
+                        Text = "В каком месяце в США традиционно начинаются президентские выборы?",
+                        IssuePrice = 200,
+                        Answers = new List<Answer>
+                     {
+                         new Answer{ Text = "Август", IsCorrect = false},
+                         new Answer{ Text = "Октябрь", IsCorrect = false},
+                         new Answer{ Text = "Ноябрь", IsCorrect = true},
+                         new Answer{ Text = "Февраль", IsCorrect = false}
+                     }
+                    };
+                    Question quest9 = new Question
+                    {
+                        Text = "Какого персонажа не было среди Бременских музыкантов??",
+                        IssuePrice = 200,
+                        Answers = new List<Answer>
+                     {
+                         new Answer{ Text = "Кот", IsCorrect = false},
+                         new Answer{ Text = "Гусь", IsCorrect = true},
+                         new Answer{ Text = "Собака", IsCorrect = false},
+                         new Answer{ Text = "Осел", IsCorrect = false}
+                     }
+                    };
+                    Question quest10 = new Question
+                    {
+                        Text = "В какое время года сутки короче?",
+                        IssuePrice = 200,
+                        Answers = new List<Answer>
+                     {
+                         new Answer{ Text = "Летом", IsCorrect = false},
+                         new Answer{ Text = "Зимой", IsCorrect = false},
+                         new Answer{ Text = "Весной", IsCorrect = false},
+                         new Answer{ Text = "Одинаковые", IsCorrect = true}
+                     }
+                    };
+                    Question quest11 = new Question
+                    {
+                        Text = "Сколько государств участвовало во Второй мировой войне?",
+                        IssuePrice = 200,
+                        Answers = new List<Answer>
+                     {
+                         new Answer{ Text = "53", IsCorrect = false},
+                         new Answer{ Text = "61", IsCorrect = true},
+                         new Answer{ Text = "72", IsCorrect = false},
+                         new Answer{ Text = "75", IsCorrect = false}
+                     }
+                    };
+                    Question quest12 = new Question
+                    {
+                        Text = "Самая крупная рыба",
+                        IssuePrice = 200,
+                        Answers = new List<Answer>
+                     {
+                         new Answer{ Text = "Белая акула", IsCorrect = false},
+                         new Answer{ Text = "Китовая акула", IsCorrect = true},
+                         new Answer{ Text = "Голубой кит", IsCorrect = false},
+                         new Answer{ Text = "Синий кит", IsCorrect = false}
+                     }
+                    };
+                    #endregion
+                    #region Add & Save
+                    context.Questions.Add(quest1);
+                    context.Questions.Add(quest2);
+                    context.Questions.Add(quest3);
+                    context.Questions.Add(quest4);
+                    context.Questions.Add(quest5);
+                    context.Questions.Add(quest6);
+                    context.Questions.Add(quest7);
+                    context.Questions.Add(quest8);
+                    context.Questions.Add(quest9);
+                    context.Questions.Add(quest10);
+                    context.Questions.Add(quest11);
+                    context.Questions.Add(quest12);
+                    #endregion
+
+
+                    /*
+                    Question quest = new Question
+                    {
+                        Text = "",
+                        IssuePrice = 200,
+                        Answers = new List<Answer>
+                     {
+                         new Answer{ Text = "", IsCorrect = false},
+                         new Answer{ Text = "", IsCorrect = false},
+                         new Answer{ Text = "", IsCorrect = false},
+                         new Answer{ Text = "", IsCorrect = false}
+                     }
+                    };
+                    context.Questions.Add(quest);
+                    
+                     */
+                    context.SaveChanges();
                 }
 
                 catch (DbUpdateException ex)
@@ -49,30 +217,26 @@ namespace My_Game
         //РЕГИСТРАЦИЯ
         public static Account Registration(string login, string password)
         {
-            using (MyContext db = new MyContext())
+            using (MyContext context = new MyContext())
             {
                 try
                 {
-                    Account JmixAcc = new Account
+                    Account newAkk = new Account
                     {
                         Login = login,
-                        //128-битный алгоритм хеширования md5
                         Password = MD5Hash.GetMd5Hash(password),
                         Personal = new Personal_Data_Acc
                         {
-                            DateOfBirth = DateTime.Now,
-                            Email = "you@mail.com",
-                            Name = "Name",
-                            Patronymic = "Patronymic",
-                            Surname = "Surname",
-                            Photo = ImageToByte(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + @"\Images\milionaireLogoBackground.png")
+                            Name="",
+                            Surname = "",
+                            Patronymic ="",
+                            Email ="",
+                            Photo = ImageToByte(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + @"\Images\NoImage.png")
                         }
                     };
-                    // добавляю в бд
-                    db.Accounts.Add(JmixAcc);
-                    db.Entry(JmixAcc).State = EntityState.Added;
-                    db.SaveChanges();
-                    return JmixAcc;
+                    context.Accounts.Add(newAkk);
+                    context.SaveChanges();
+                    return newAkk;
                 }
 
                 catch (DbUpdateException ex)
@@ -88,7 +252,7 @@ namespace My_Game
             using (var context = new MyContext())
             {
                 Account akk = context.Accounts.Include(a => a.Personal).FirstOrDefault(a => a.Login == login);
-                if ((akk != null)&& (MD5Hash.VerifyMd5Hash(password, akk.Password)))
+                if ((akk != null) && (MD5Hash.VerifyMd5Hash(password, akk.Password)))
                     return akk;
                 else
                     return null;
@@ -125,7 +289,7 @@ namespace My_Game
         }
 
         //Добавление и Сохранение дополнительных данных
-        public static bool SaveAdditionalInfo(int id, string path="", string name = "", string surname = "", string patronymic = "", string email = "")
+        public static bool SaveAdditionalInfo(int id, string path = "", string name = "", string surname = "", string patronymic = "", string email = "")
         {
             MyContext context = new MyContext();
             IEnumerable<Account> acc = context.Accounts
@@ -140,19 +304,18 @@ namespace My_Game
                     c.Personal.Patronymic = patronymic;
                     c.Personal.Email = email;
                     if (path != ByteToImage(c.Personal.Photo).ToString())
-                    {
                         c.Personal.Photo = ImageToByte(path);
-                    }
                     return c;
                 });
             foreach (Account customer in acc)
                 context.Entry(customer).State = EntityState.Modified;
 
             context.SaveChanges();
-            
+
             return true;
         }
 
+        //Конвертер из картинки в байты
         public static byte[] ImageToByte(string path)
         {
 
@@ -170,6 +333,7 @@ namespace My_Game
             return null;
         }
 
+        //Конвертер из байтов в картинки
         public static BitmapImage ByteToImage(byte[] data)
         {
             BitmapImage imgsource = new BitmapImage();
@@ -180,20 +344,35 @@ namespace My_Game
             return imgsource;
         }
 
-        //public static Question GetRandQuestion(string login, string password)
-        //{
-        //    using (var ctx = new MyContext())
-        //    {
-        //        //Выборка из базы данных, пользователя с логином, принимаемым в функции
-        //        var quest = (from s in ctx.Accounts
-        //                     where s.Login == login
-        //                     select s).FirstOrDefault<Account>();
+        //Получить случайный вопрос цены price (внутри вопроса есть 4 ответа)
+        public static Question GetQuestion(int price)
+        {
+            if (price >= 100)
+            {
+                using (var context = new MyContext())
+                {
+                    int min = 0, max = 0;
+                    var question = context.Questions.Where(c => c.IssuePrice == price);
+                    foreach (var qst in question)
+                    {
+                        if (max == 0)
+                            min = qst.Id;
+                        max = qst.Id;
+                    }
+                    return context.Questions.Find(Rand(min, max));
+                }
+            }
 
+            else
+                return null;
+        }
 
-        //        return quest;
-        //    }
+        private static int Rand(int min, int max)
+        {
+            Random rnd = new Random();
+            lock (rnd)
+                return rnd.Next(min, max + 1);
+        }
 
-        //    return null;
-        //}
     }
 }
